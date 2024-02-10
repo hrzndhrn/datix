37346a4f0a6eb8513c58a75c9d294e5353fd3c74
Marcus Kruse <esurk.sucram@gmail.com>
2024-02-10 08:37:50 +0100

    Update deps

--- .recode.exs
+++ .recode.exs
@@ -1,16 +1,18 @@
 [
-  version: "0.6.4",
+  version: "0.7.2",
   # Can also be set/reset with `--autocorrect`/`--no-autocorrect`.
   autocorrect: true,
   # With "--dry" no changes will be written to the files.
   # Can also be set/reset with `--dry`/`--no-dry`.
   # If dry is true then verbose is also active.
   dry: false,
+  # Enables or disables color in the output.
+  color: true,
   # Can also be set/reset with `--verbose`/`--no-verbose`.
   verbose: false,
   # Can be overwritten by calling `mix recode "lib/**/*.ex"`.
   inputs: ["{mix,.formatter,.recode}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  formatter: {Recode.Formatter, []},
+  formatters: [Recode.CLIFormatter],
   tasks: [
     # Tasks could be added by a tuple of the tasks module name and an options
     # keyword list. A task can be deactivated by `active: false`. The execution of
